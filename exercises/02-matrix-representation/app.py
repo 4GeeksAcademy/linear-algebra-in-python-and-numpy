6beb8881d5a931f8c54d85d1568eb58f901e75cf
Cerco01 <axelinvest@icloud.com>
2026-04-09 19:06:19 +0200

    feat: complete all 8 exercises of linear algebra module

--- exercises/02-matrix-representation/app.py
+++ exercises/02-matrix-representation/app.py
@@ -2,18 +2,10 @@ import numpy as np  # Import NumPy to use it later
 import random  # To generate random values in pure Python
 
 def create_matrix(n, m, mode="pure"):
-    """
-    Generates an n x m matrix with random values.
-
-    Parameters:
-    - n: int -> Number of rows.
-    - m: int -> Number of columns.
-    - mode: str -> "pure" for Python lists, "numpy" for NumPy arrays.
-
-    Returns:
-    - A matrix in the specified format.
-    """
-    pass # Remove this line when implemented
+    lst = []
+    if mode == "numpy":
+        return np.random.randint(0, 100, size=(n, m))
+    return [[random.randint(0, 100) for _ in range(m)] for _ in range(n)]
 
 # Test the function with the following values:
 matrix1 = create_matrix(3, 3, "pure")
--- exercises/07-solving-systems-of-linear-equations/app.py
+++ exercises/07-solving-systems-of-linear-equations/app.py
@@ -1,17 +1,26 @@
 import numpy as np  
 
 def solve_system(A, b, mode="numpy"):
-    """
-    Solves the linear system Ax = b.
+    "Solves the linear system Ax = b."
 
-    Parameters:
-    - A: list of lists or np.array -> Coefficient matrix.
-    - b: list or np.array -> Result vector.
-    - mode: str -> "pure" for manual computation, "numpy" for np.linalg.solve().
+    if mode == 'pure':
+        det = (A[0][0] * A[1][1]) - (A[0][1] * A[1][0])
 
-    Returns:
-    - The solution vector x.
-    """
-   
-    pass  # Remove this line when implemented
+        if det == 0:
+            return "not invertible"
+        else:
+            inv = [[A[1][1] / det, -A[0][1] / det], [-A[1][0] / det, A[0][0] / det]]
 
+        return [sum(inv[i][k] * b[k] for k in range(len(b))) for i in range(len(inv))]
+
+    else:
+        A = np.array(A)
+        b = np.array(b)
+        return np.linalg.solve(A, b)
+
+
+A = [[2, 3], [4, -1]]
+b = [5, 1]
+
+print(solve_system(A, b, "pure"))
+print(solve_system(A, b, "numpy"))
